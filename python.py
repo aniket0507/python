2947391a7acb7ac44b9df1af7b574f4334199b19
aniket0507 <43309753+aniket0507@users.noreply.github.com>
2020-07-28 15:11:11 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2,6 +2,6 @@ movies = ["shawshank redemption", "Inception", "Forest Gump"]
 
 print(movies)
 
-celebrities = ["johny Depth", "michael henry","sushant singh rajput"]
+celebrities = ["johny Depth", "michael henry","sushant singh rajput","mark henry"]
 
 print(celebrities)
